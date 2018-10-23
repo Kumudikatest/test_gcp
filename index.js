86e38d6d52c960c0a288bd7477e1d63935082abe
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2018-10-23 17:46:59 +0530

    test1

---
+++ index.js
@@ -0,0 +1,4 @@
+// This file is used to register all your cloud functions in GCP.
+// DO NOT EDIT/DELETE THIS, UNLESS YOU KNOW WHAT YOU ARE DOING.
+
+exports.testgcpfunction = require("./test_gcp/function.js").handler;
